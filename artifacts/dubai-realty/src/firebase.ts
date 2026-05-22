60a69df219685cb1dbb7eb6d30791e6d6dfd90ae
1fffqwe123-cell <1fff.qwe123@gmail.com>
2026-05-23 02:31:27 +0300

    Update firebase.ts

--- artifacts/dubai-realty/src/firebase.ts
+++ artifacts/dubai-realty/src/firebase.ts
@@ -1,9 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "../firebase-applet-config.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAgvKBwLfwhKwQDLMH158mmzFT7cxlvL4",
+  authDomain: "youthful-current-f8gvj.firebaseapp.com",
+  projectId: "youthful-current-f8gvj",
+  storageBucket: "youthful-current-f8gvj.firebasestorage.app",
+  messagingSenderId: "690559924735",
+  appId: "1:690559924735:web:c3bfa839d5159cb13ecfca"
+};
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-
-export default app;
